d5d838808e879a353ba01dc4da07f7bfd5acfad4
Jaosuzart <joaomarcelosuzartcastro@gmail.com>
2025-08-11 10:20:42 -0300

    Enviando atualizacao ao github

--- script/script.js
+++ script/script.js
@@ -180,3 +180,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+document.querySelectorAll(".faq-question").forEach((question) => {
+  question.addEventListener("click", () => {
+    const item = question.parentElement;
+    item.classList.toggle("active");
+  });
+});
